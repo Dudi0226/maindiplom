a1208d5c7313af3ef3b34b6c365ebf5e065993ff
Dudi0226 <96303541+Dudi0226@users.noreply.github.com>
2024-05-20 01:39:10 +0800

    upate

--- src/routes/news.Route.js
+++ src/routes/news.Route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { createNews, getNews } = require("../controllers/news_controller");
 const { protect } = require("../middleware/protect");
-router.route("/").get(protect, getNews);
+router.route("/").get(getNews);
 router.route("/create").post(protect, createNews);
 module.exports = router;
